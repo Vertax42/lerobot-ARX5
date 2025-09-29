a7647cb8fef80f079cc90267dbf822feab8fbd74
Vertax42 <1007644658@qq.com>
2025-09-29 18:09:45 +0800

    remove run set can

--- run_set_can.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# 包装脚本：运行set_can.sh而不显示libtinfo警告
-
-# 使用env -i创建干净的环境，避免conda库冲突
-env -i PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin bash -c "
-cd /home/ubuntu/lerobot-ARX5
-./set_can.sh
-" 2>/dev/null
